ec8bc57faf030ee956d57dc5536df6f6f2130499
isalexlog <--global>
2020-07-10 19:38:28 +0200

    extracting config parameters into a config file - first commit

--- weather-fetch/script.js
+++ weather-fetch/script.js
@@ -1,18 +1,15 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather';
-const APPID = 'bed156d40174100c73406417523ddaf1';
+fetch('/weather-fetch/config.json')
+    .then(response => response.json())
+    .then(config => config)
+    .catch(error => console.log(error));
 
-const weatherFieldConig = [
-    {name: 'Temperature', field: 'main.temp'},
-    {name: 'Wind deg', field: 'wind.deg'},
-    {name: 'Wind speed', field: 'wind.speed'}
-];
 
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     const country = event.target['country'].value;
     const city = event.target['city'].value;
   
-    fetch(`${URL}?country=${country}&q=${city}&APPID=${APPID}`)
+    fetch(`${config.URL}?country=${country}&q=${city}&APPID=${config.APPID}`)
         .then(response => response.json())
         .then(json => showWeather(json, event.target));
 })
@@ -23,7 +20,7 @@ function showWeather(json, target) {
     titleElement$.innerText = `Weather for ${json.name}`;
     
     let html = '';
-    weatherFieldConig.forEach((el => {
+    config.weatherFieldConig.forEach((el => {
         html += '<div class="row">';
         html += `<div class="col-6">${el.name}</div><div class="col-6">${eval('json.' + el.field)}</div>`
         html += '</div>';
